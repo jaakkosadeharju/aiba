2e4eac0e7568ce9b3e603e971d96a038302205c1
Jukka <cts-sweng2@sil.org.pg>
2019-11-19 11:42:45 +1000

    Primitive changes

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 
 var gameData = {
   area: {
     size: {
-      width: 1000,
-      height: 1000
+      width: 900,
+      height: 660
     }
   },
   teams: [
@@ -14,23 +15,23 @@ var gameData = {
       name: 'Junttaajat',
       color: '#00ff00',
       players: [
-        // game area 1000x1000
-        { id: 1, name: "Player 1a", pos: { x: 250, y: 300 } },
-        { id: 2, name: "Player 2a", pos: { x: 250, y: 400 } },
-        { id: 3, name: "Player 3a", pos: { x: 250, y: 500 } },
-        { id: 4, name: "Player 4a", pos: { x: 250, y: 600 } },
-        { id: 5, name: "Player 5a", pos: { x: 250, y: 700 } },
+        // game area 900x660
+        { id: 1, name: "Player 1a", pos: { x: 225, y: 110 } },
+        { id: 2, name: "Player 2a", pos: { x: 225, y: 220 } },
+        { id: 3, name: "Player 3a", pos: { x: 225, y: 330 } },
+        { id: 4, name: "Player 4a", pos: { x: 225, y: 440 } },
+        { id: 5, name: "Player 5a", pos: { x: 225, y: 550 } },
       ]
     },
     {
       name: 'Sandibar',
       color: '#ff0000',
       players: [
-        { id: 11, name: "Player 1b", pos: { x: 750, y: 300 } },
-        { id: 12, name: "Player 2b", pos: { x: 750, y: 400 } },
-        { id: 13, name: "Player 3b", pos: { x: 750, y: 500 } },
-        { id: 14, name: "Player 4b", pos: { x: 750, y: 600 } },
-        { id: 15, name: "Player 5b", pos: { x: 750, y: 700 } },
+        { id: 11, name: "Player 1b", pos: { x: 675, y: 110 } },
+        { id: 12, name: "Player 2b", pos: { x: 675, y: 220 } },
+        { id: 13, name: "Player 3b", pos: { x: 675, y: 330 } },
+        { id: 14, name: "Player 4b", pos: { x: 675, y: 440 } },
+        { id: 15, name: "Player 5b", pos: { x: 675, y: 550 } },
       ]
     }
   ]
@@ -39,6 +40,8 @@ var gameData = {
 setInterval(() => {
   io.emit('new positions', JSON.stringify(gameData.teams));
 }, 1000);
+
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html');
