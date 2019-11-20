4b57730fd22943b116dd03bf50858679633e3ca8
Jaakko Sadeharju <jaakko.sadeharju@gmail.com>
2019-11-20 08:04:43 +0200

    Introduce team and position. Small refactoring.

---
+++ game_objects/position.js
@@ -0,0 +1,10 @@
+class Position {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  // TODO: helper methods e.g. distanceTo(pos), directionTo(pos)...
+}
+
+exports.Position = Position;
--- index.js
+++ index.js
@@ -3,7 +3,9 @@ var app = express();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 var Ball = require('./game_objects/ball').Ball;
+var Team = require('./game_objects/team.js').Team;
 var Player = require('./game_objects/player.js').Player;
+var Position = require('./game_objects/position.js').Position;
 
 const areaHeight = 660, areaWidth = 940, playerSize = 10, ballSize = 5, goalWidth = 240;
 
@@ -35,41 +37,35 @@ var gameData = {
     color: '#000000'
   },
   teams: [
-    {
-      id: 0,
+    new Team({
+      id: 1,
       name: 'Junttaajat',
       color: '#00ff00',
       players: [
         // game area 900x660
-        { id: 11, name: "Ndumiso Nkadimeng", pos: { x: areaWidth/4, y: 1*areaHeight/6 }, },
-        { id: 12, name: "Emlyn Hukkes", pos: { x: areaWidth/4, y: 2*areaHeight/6 } },
-        { id: 13, name: "Sossermaster", pos: { x: areaWidth/4, y: 3*areaHeight/6 } },
-        { id: 14, name: "Player 4a", pos: { x: areaWidth/4, y: 4*areaHeight/6 } },
-        { id: 15, name: "Player 5a", pos: { x: areaWidth/4, y: 5*areaHeight/6 } },
+        new Player({id: 11, name: "Ndumiso Nkadimeng", position: new Position(areaWidth/4, 1*areaHeight/6), speed: 34, stamina: 33, power: 33 }),
+        new Player({id: 12, name: "Emlyn Hukkes", position: new Position(areaWidth/4, 2*areaHeight/6), speed: 34, stamina: 33, power: 33 }),
+        new Player({id: 13, name: "Sossermaster", position: new Position(areaWidth/4, 3*areaHeight/6), speed: 34, stamina: 33, power: 33 }),
+        new Player({id: 14, name: "Player 4a", position: new Position(areaWidth/4, 4*areaHeight/6), speed: 34, stamina: 33, power: 33 }),
+        new Player({id: 15, name: "Player 5a", position: new Position(areaWidth/4, 5*areaHeight/6), speed: 34, stamina: 33, power: 33 }),
       ]
-    },
-    {
-      id: 1,
+    }),
+    new Team({
+      id: 2,
       name: 'Sandibar',
       color: '#ff0000',
       players: [
-        { id: 21, name: "Player 1b", pos: { x: 3*areaWidth/4, y: 1*areaHeight/6 } },
-        { id: 22, name: "Player 2b", pos: { x: 3*areaWidth/4, y: 2*areaHeight/6 } },
-        { id: 23, name: "Player 3b", pos: { x: 3*areaWidth/4, y: 3*areaHeight/6 } },
-        { id: 24, name: "Player 4b", pos: { x: 3*areaWidth/4, y: 4*areaHeight/6 } },
-        { id: 25, name: "Player 5b", pos: { x: 3*areaWidth/4, y: 5*areaHeight/6 } },
+        new Player({id: 21, name: "Player 1b", position: new Position(3*areaWidth/4, 1*areaHeight/6), speed: 34, stamina: 33, power: 33 }),
+        new Player({id: 22, name: "Player 2b", position: new Position(3*areaWidth/4, 2*areaHeight/6), speed: 34, stamina: 33, power: 33 }),
+        new Player({id: 23, name: "Player 3b", position: new Position(3*areaWidth/4, 3*areaHeight/6), speed: 34, stamina: 33, power: 33 }),
+        new Player({id: 24, name: "Player 4b", position: new Position(3*areaWidth/4, 4*areaHeight/6), speed: 34, stamina: 33, power: 33 }),
+        new Player({id: 25, name: "Player 5b", position: new Position(3*areaWidth/4, 5*areaHeight/6), speed: 34, stamina: 33, power: 33 }),
       ]
-    }
+    })
   ]
 };
 
 const players = [];
-
-gameData.teams.forEach(team => {
-  team.players.forEach(player => {
-    players.push(new Player(player.id, player.id[0], player.name, player.pos, 33, 33, 33));
-  });
-});
 
 setInterval(() => {
   io.emit('new positions', JSON.stringify(gameData.teams));
