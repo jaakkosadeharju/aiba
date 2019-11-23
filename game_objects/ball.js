35628b5ef8eb598da9019e26eb72579bf8affddf
JukkaSad <JukkaSad@users.noreply.github.com>
2019-11-23 18:59:38 +0200

    Ball movement and kick implemented. Not very well yet. (#8)

--- game_objects/ball.js
+++ game_objects/ball.js
@@ -1,11 +1,15 @@
 var Position = require('./position.js').Position;
 
 class Ball {
-    constructor(size, position, direction, speed, color, gameArea) {
+    constructor(size, position, direction, color, gameArea) {
     	this.size = size;
+        this.weight = 1;
         this.position = position;
         this.direction = direction;
-        this.speed = speed;
+        this.speed = {
+            x: 0,
+            y: 0
+        }
         this.color = color;
         this.gameArea = gameArea;
         this.controlledBy = null;
@@ -15,19 +19,45 @@ class Ball {
         this.controlledBy = player;
     }
 
-    //Kick action resulting movment
-    //direction: direction of the kick
-    //power: desired power to kick.
-    kick(direction, power) {
-
-    }
-
 	//Set the new position
 	//TODO: Apply slowing forces if not controlled
-	move(dt) {
+	move(dt, direction, speed) {
+
 		if (this.controlledBy != null) {
-			this.position = new Position(this.controlledBy.position.x + (this.size+this.controlledBy.size)/2, this.controlledBy.position.y);
+            this.speed.x = 0;
+            this.speed.y = 0;
+			this.position = new Position(this.controlledBy.position.x + (this.size+this.controlledBy.size), this.controlledBy.position.y);
 		}
+        else {
+
+            this.direction = direction;
+            this.speed.x = Math.cos(direction) * speed;
+            this.speed.y = Math.sin(direction) * speed;
+            console.log("Speed: "+this.speed.x + " "+ this.speed.y);
+            let newX = this.position.x + this.speed.x;
+            let newY = this.position.y + this.speed.y;
+
+            // limit x coordinate to game area
+            if (newX > this.gameArea.size.width && !this.gameArea.goals.some(goal => goal.isInside(this.position))) {
+                newX = this.gameArea.size.width;
+                this.direction = this.direction + Math.PI; //bounce
+            }
+            else if (newX < 0 && !this.gameArea.goals.some(goal => goal.isInside(this.position))) {
+                newX = 0;
+                this.direction = this.direction + Math.PI;
+            }
+            // limit y coordinate to game area
+            if (newY > this.gameArea.size.height && !this.gameArea.goals.some(goal => goal.isInside(this.position))) { //toimiiko tämä some?
+                newY = this.gameArea.size.height;
+                this.direction = this.direction + Math.PI;
+            }
+            else if (newY < 0 && !this.gameArea.goals.some(goal => goal.isInside(this.position))) {
+                newY = 0;
+                this.direction = this.direction + Math.PI;
+            }
+
+            this.position = new Position(newX, newY);
+        }
 	}
 
 }
